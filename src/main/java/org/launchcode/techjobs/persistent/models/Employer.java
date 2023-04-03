99b4fff9123f4e3326df47b3b16746ac4ccb6003
stlstitch <jamesbm1983@gmail.com>
2023-04-02 23:38:34 -0500

    Version 5.0
    Updated Employer.java and Job.java files

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,11 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "job_id")
+    private List<Job> jobs = new ArrayList<>();
 
     @NotNull
     @Size(max = 100, min = 25, message = "Sorry entry must be between 25 and 100 characters long.")

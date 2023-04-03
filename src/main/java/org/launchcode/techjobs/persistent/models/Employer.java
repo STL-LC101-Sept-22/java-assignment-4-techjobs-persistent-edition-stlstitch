eaff1550b836501898d479ad6e5a3efbdf8d7a06
stlstitch <jamesbm1983@gmail.com>
2023-04-02 21:34:08 -0500

    Version 1.0
    Database Connected
    Models and Abstract Entity added.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    private String location;
 
+    public Employer() {
+
+    }
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 500, min = 25, message = "Sorry entry must be between 25 and 500 characters long.")
+    private String description;
+
+    public Skill() {
+
+    }
+
+    public Skill(String skill) {
+        super();
+        this.description = skill;
+    }
+
+    public String getSkill() {
+        return description;
+    }
+
+    public void setSkill(String skill) {
+        this.description = skill;
+    }
 }
